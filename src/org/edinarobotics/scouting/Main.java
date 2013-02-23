670a072c1164059f8a03643ebca321371bca6eaa
Alex O'Neill <oneill.alexander.b@gmail.com>
2013-02-23 14:31:48 -0600

    Initial commit

---
+++ src/org/edinarobotics/scouting/Main.java
@@ -0,0 +1,18 @@
+package org.edinarobotics.scouting;
+
+import org.edinarobotics.scouting.gui.TestJFrame;
+
+/**
+ *
+ * @author aoneill
+ */
+public class Main {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) 
+  {
+    new TestJFrame();
+  }
+}
