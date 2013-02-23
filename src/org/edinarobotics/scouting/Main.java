6d877b7f7e25ffcac08c385dfc02e1024bc36416
Alex O'Neill <oneill.alexander.b@gmail.com>
2013-02-23 14:47:51 -0600

    Removed TestJFrame for continued development

--- src/org/edinarobotics/scouting/Main.java
+++ src/org/edinarobotics/scouting/Main.java
@@ -1,7 +1,5 @@
 package org.edinarobotics.scouting;
 
-import org.edinarobotics.scouting.gui.TestJFrame;
-
 /**
  *
  * @author aoneill
@@ -13,6 +11,6 @@ public class Main {
    */
   public static void main(String[] args) 
   {
-    new TestJFrame();
+    
   }
 }
